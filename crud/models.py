b8a228c4f5e84e32653b9ff760f12684d390c036
masek2004 <gsmasek004@gmail.com>
2023-03-02 03:50:26 -0500

    initial commit

---
+++ crud/models.py
@@ -0,0 +1,9 @@
+from django.db import  models
+class students(models.Model):
+    name=models.CharField(max_length=30, blank=False, null=False)
+    email=models.EmailField()
+    age=models.IntegerField()
+    gender=models.CharField(max_length=30, blank=False, null=False)
+
+def __str__(self):
+    return self.name
